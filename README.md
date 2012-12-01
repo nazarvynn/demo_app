945b43151e52efa1c042f483abae08a21d749e3a
nazarvynn <nazar.vynn@gmail.com>
2012-12-01 20:56:01 +0200

    add Demo Application

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ruby on Rails Tutorial: Demo Application
+
+This is the Demo Application
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'cbdb785e2165f4bd75b647cba1a221e37ed8d03593dba1bb067fa018d612ef1eb59b2adcfcb127f4df3b377ef0b4cffcf221ead3b1f5962bf7809111fbf554ff'
